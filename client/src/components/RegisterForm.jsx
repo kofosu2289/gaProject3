11bd7858628aff1770056460ea8262da600adddf
Carol Park <carol@Carols-MacBook.local>
2019-07-22 15:13:10 -0400

    create react app and registerform

---
+++ client/src/components/RegisterForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default (props) => (
+  <>
+  <h3>Register Form</h3>
+  <form onSubmit={props.handleSubmit}>
+    <label
+      htmlFor="name">Name</label>
+    <input
+      type="text"
+      name="name"
+      value={props.registerForm.name}
+      id="name"
+      onChange={props.handleChange} />
+    <label
+      htmlFor="password">Password</label>
+    <input
+      type="password"
+      name="password"
+      value={props.registerForm.password}
+      id="password"
+      onChange={props.handleChange} />
+    <input type="submit" value="Sign Up!" />
+  </form>
+  </>
+);
