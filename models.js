1ef8b8626bc94d901c8524da28dce308f0732c3a
Carol Park <carol@Carols-MacBook.local>
2019-07-22 11:34:06 -0400

    create db, models hello.length;

---
+++ models.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+  database: 'benjamins_db',
+  dialect: 'postgres',
+  define: {
+    underscored: true,
+  }
+})
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  email: Sequelize.STRING,
+  pw_hash: Sequelize.STRING,
+})
+
+const Category = sequelize.define('category', {
+  name: Sequelize.STRING,
+  image_url: Sequelize.STRING,
+})
+
+const Product = sequelize.define('product', {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  image_url: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+})
+
+
+//JOIN TABLE
+Product.belongsTo(User, {through: 'shopping_cart'});
+
+
+User.hasMany(Category);
+Category.belongsTo(User);
+Category.hasMany(Product, { onDelete: 'cascade' }); 
+Product.belongsTo(Category);
+
+module.exports = {
+  sequelize,
+  User,
+  Category,
+  Product
+}
+
---
+++ scratch.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const BASE_URL = 'http://localhost:3001'
+
+
+
+const main = () => {
+
+};
+
+main();
