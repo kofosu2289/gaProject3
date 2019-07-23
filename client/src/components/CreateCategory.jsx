47deadbb79dfa37b51d45a9d74c3ee83be367dfb
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-07-23 10:03:24 -0400

    components

---
+++ client/src/components/CreateCategory.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class CreateCategory extends React.Component {
+}
+
+return (
+  <div className="searchbar">
+    <input id="search"
+      type="text"
+      name="search"
+      placeholder="SEARCH">
+    </input>
+  </div>
+  <div className="catForm"></div>
+  <h1>ADD CATEGORY</h1>
+  <input id="catName"
+    type="text"
+    name="category-name"
+    placeholder="CATEGORY NAME">
+  </input>
+
+  <input id="catImg"
+    type="url"
+    name="category-img"
+    placeholder="CATEGORY IMAGE">
+  </input>
+  <button id="createcat">SUBMIT</button>
+    )
+export default CreateCategory;
+
+
+
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Nav() {
+  return (
+    <div className="app-nav">
+      <h4>This is where the nav will go</h4>
+    </div>
+  )
+}
