811ce3c54fd5e0c8a04bb3a360cdab2ff4b15fcf
CeeCee <ceecee@CeeCees-MacBook-Air.local>
2019-07-23 10:47:40 -0400

    component changes

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+
+export default function Cart() {
+  return (
+    <div className="cart-logo">
+      {/* <Link to="/"> */}
+      <img id="logo" src="http://oi66.tinypic.com/311wbbd.jpg" alt="cartlogo" />
+      {/* </Link> */}
+    </div>
+  )
+}
