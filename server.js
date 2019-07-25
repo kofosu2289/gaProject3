a9fb2763152ff3740591b543ceb2fc17ab94e097
Kenneth Ofosu <kofosu89@gmail.com>
2019-07-25 16:58:18 -0400

    new server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const userRouter = require('./routes/userRouter');
 const productRouter = require('./routes/productRouter');
